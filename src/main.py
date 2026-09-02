970ae794e79d09b9fc3f235827cce1ccf15dccf8
Alexander Najy <Alexander.najy@gmail.com>
2026-09-02 07:52:29 -0700

    added ml tagging python script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from build_classifications import build_classifications
 from build_tracker import build_tracker
+from build_ml_tags import build_ml_tags
 from build_site_data import build_site_data
 
 def main():
     print("Running full pipeline...")
     build_classifications()
     build_tracker()
+    build_ml_tags()
     build_site_data()
     print("Done.")
 
